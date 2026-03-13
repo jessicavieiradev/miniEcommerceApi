6812e3425c2af3c3bef7c89866a4a0f2cd1093dd
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-03-13 11:44:45 -0300

    feat: configure DbContext by adding entity mappings to OnModelCreating

--- miniEcommerceApi/Data/AppDbContext.cs
+++ miniEcommerceApi/Data/AppDbContext.cs
@@ -5,13 +5,18 @@ namespace miniEcommerceApi.Data
 {
     public sealed class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         DbSet<Adresses> Adresses { get; set; }
         DbSet<Categories> Categories { get; set; }
         DbSet<Costumers> Customers { get; set; }
         DbSet<Orders> Orders { get; set; }
         DbSet<Products> Products { get; set; }
-        DbSet<OrderItem> OrderItem { get; set;  }
-        DbSet<Users> Users { get; set; }
+        DbSet<OrderItem> OrderItem { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
     }
 }
