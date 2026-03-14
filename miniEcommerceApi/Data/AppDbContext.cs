6a991b048e44399bceff782250dd7ac67ed40fac
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-03-14 11:18:19 -0300

    fix(data): make DbSet properties public in AppDbContext

--- miniEcommerceApi/Data/AppDbContext.cs
+++ miniEcommerceApi/Data/AppDbContext.cs
@@ -6,12 +6,12 @@ namespace miniEcommerceApi.Data
     public sealed class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        DbSet<Adresses> Adresses { get; set; }
-        DbSet<Categories> Categories { get; set; }
-        DbSet<Costumers> Customers { get; set; }
-        DbSet<Orders> Orders { get; set; }
-        DbSet<Products> Products { get; set; }
-        DbSet<OrderItem> OrderItem { get; set; }
+        public DbSet<Adresses> Adresses { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<Costumers> Customers { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<Products> Products { get; set; }
+        public DbSet<OrderItem> OrderItem { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
