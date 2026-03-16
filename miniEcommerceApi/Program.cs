38bea750b8f17bb503fe295c69b7ecd8dd66c720
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-03-16 12:00:01 -0300

    feat: add seeder confuration to program

--- miniEcommerceApi/Program.cs
+++ miniEcommerceApi/Program.cs
@@ -1,19 +1,31 @@
 using Microsoft.EntityFrameworkCore;
 using miniEcommerceApi.Data;
+using miniEcommerceApi.Data.Seeders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+Console.WriteLine(">>> STRING DE CONEXÃO CARREGADA: " + connectionString);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 builder.Services.AddControllers();
 
+builder.Services.AddHttpClient();
+
+builder.Services.AddScoped<ProductsSeeder>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+using(var scope = app.Services.CreateScope())
+{    
+    var seeder = scope.ServiceProvider.GetRequiredService<ProductsSeeder>();
+    await seeder.SeedAsync();
+}
 
 app.UseHttpsRedirection();
 
