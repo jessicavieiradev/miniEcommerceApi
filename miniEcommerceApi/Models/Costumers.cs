f52ef22fbc783244573e23bb111c88beba5658db
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-03-17 19:33:50 -0300

    chore: add parameterless constructor to Costumer entity

--- miniEcommerceApi/Models/Costumers.cs
+++ miniEcommerceApi/Models/Costumers.cs
@@ -3,9 +3,22 @@
     public class Costumers
     {
         public Guid Id { get; set; }
+        public Guid UserId { get; set; } 
+        public Users User { get; set; } 
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public string Cpf { get; set; } = string.Empty;
+
+        public Costumers() { }
+        public Costumers(Guid userId, string firstName, string lastName, string phone, string cpf)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            FirstName = firstName;
+            LastName = lastName;
+            Phone = phone;
+            Cpf = cpf;
+        }
     }
 }
