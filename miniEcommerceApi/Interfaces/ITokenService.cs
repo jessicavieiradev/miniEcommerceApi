d17d28b2c727d2f82291e7d5cde0a01a8811db64
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-03-17 19:27:00 -0300

    feat: add ITokenService interface and TokenService implementation

---
+++ miniEcommerceApi/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using miniEcommerceApi.Models;
+
+namespace miniEcommerceApi.Interfaces
+{
+    public interface ITokenService
+    {
+        Task<string> GenerateToken(Users user, string name);
+    }
+}
---
+++ miniEcommerceApi/Services/TokenService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using miniEcommerceApi.Interfaces;
+using miniEcommerceApi.Models;
+using System.Security.Claims;
+using System.Text;
+
+namespace miniEcommerceApi.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<Users> _userManager;
+
+        public TokenService(IConfiguration configuration, UserManager<Users> userManager)
+        {
+            _configuration = configuration;
+            _userManager = userManager;
+        }
+
+        public async Task<string> GenerateToken(Users user, string name)
+        {
+            var handler = new JsonWebTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]!);
+            var roles = await _userManager.GetRolesAsync(user);
+            var expiracaoMinutos = double.Parse(_configuration["Jwt:AudienceInMinutes"] ?? "60");
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Name, name),
+            };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var descriptor = new SecurityTokenDescriptor
+            {
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"],
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(expiracaoMinutos),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+
+            };
+            string token = handler.CreateToken(descriptor);
+            return token;
+        }
+    }
+}
