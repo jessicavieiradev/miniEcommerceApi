b28051208679f1203328871bd7f9fdbfaa8d47ab
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-03-20 10:56:35 -0300

    fix: rename Costumer to Customer in IAuthService

--- miniEcommerceApi/Interfaces/IAuthService.cs
+++ miniEcommerceApi/Interfaces/IAuthService.cs
@@ -1,16 +1,16 @@
 ﻿using miniEcommerceApi.DTOs.AuthDTO.Request;
-using miniEcommerceApi.DTOs.CostumersDTO.Request;
-using miniEcommerceApi.DTOs.CostumersDTO.Response;
+using miniEcommerceApi.DTOs.CustomersDTO.Request;
+using miniEcommerceApi.DTOs.CustomersDTO.Response;
 
 namespace miniEcommerceApi.Interfaces
 {
     public interface IAuthService
     {
-        Task<AuthResponse> CreateCostumer(CreateCostumerRequest dto);
-        Task UpdateCostumer(Guid id, UpdateCostumerRequest dto);
+        Task<AuthResponse> CreateCustomer(CreateCustomerRequest dto);
+        Task UpdateCustomer(Guid id, UpdateCustomerRequest dto);
         Task DeleteUser(Guid id);
-        Task<CostumerResponse> GetCostumerById(Guid id);
-        Task<IEnumerable<CostumerResponse>> GetAllCostumers();
+        Task<CustomerResponse> GetCustomerById(Guid id);
+        Task<IEnumerable<CustomerResponse>> GetAllCustomers();
         Task<AuthResponse> LoginUser(LoginRequestDTO dto);
     }
 }
