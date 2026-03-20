9cc68563e3866072d8d8392552a9fb7f98e44279
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-03-20 11:39:55 -0300

    fix: rename property name

--- miniEcommerceApi/Data/Configurations/CustomersConfig.cs
+++ miniEcommerceApi/Data/Configurations/CustomersConfig.cs
@@ -10,7 +10,7 @@ namespace miniEcommerceApi.Data.Configurations
         {
             builder.ToTable("Customers");
             builder.HasKey(x => x.Id);
-            builder.Property(x => x.FirstName).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
             builder.Property(x => x.LastName).IsRequired().HasMaxLength(100);
             builder.Property(x => x.Phone).IsRequired().HasMaxLength(20);
             builder.Property(x => x.Cpf).IsRequired().HasMaxLength(20);
--- miniEcommerceApi/Models/Customers.cs
+++ miniEcommerceApi/Models/Customers.cs
@@ -5,17 +5,17 @@
         public Guid Id { get; set; }
         public Guid UserId { get; set; } 
         public Users User { get; set; } 
-        public string FirstName { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public string Cpf { get; set; } = string.Empty;
 
         public Customers() { }
-        public Customers(Guid userId, string firstName, string lastName, string phone, string cpf)
+        public Customers(Guid userId, string Name, string lastName, string phone, string cpf)
         {
             Id = Guid.NewGuid();
             UserId = userId;
-            FirstName = firstName;
+            Name = Name;
             LastName = lastName;
             Phone = phone;
             Cpf = cpf;
