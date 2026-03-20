6db20c087bea7bff4d8bbbd2950e1533c21ac380
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-03-20 10:41:21 -0300

    feat: add Payments DbSet to AppDbContext

--- miniEcommerceApi/Data/AppDbContext.cs
+++ miniEcommerceApi/Data/AppDbContext.cs
@@ -8,12 +8,13 @@ namespace miniEcommerceApi.Data
     public sealed class AppDbContext : IdentityDbContext<Users, IdentityRole<Guid>,Guid>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        public DbSet<Adresses> Adresses { get; set; }
+        public DbSet<Addresses> Addresses { get; set; }
         public DbSet<Categories> Categories { get; set; }
-        public DbSet<Costumers> Customers { get; set; }
+        public DbSet<Customers> Customers { get; set; }
         public DbSet<Orders> Orders { get; set; }
         public DbSet<Products> Products { get; set; }
         public DbSet<OrderItem> OrderItem { get; set; }
+        public DbSet<Payments> Payments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
