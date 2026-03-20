7300910e18d555de088d028d0fb1a7520dfecb2b
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-03-20 11:16:21 -0300

    feat: add Payments property

---
+++ miniEcommerceApi/Models/Payments.cs
@@ -0,0 +1,27 @@
+﻿using miniEcommerceApi.Enums;
+
+namespace miniEcommerceApi.Models
+{
+    public class Payments
+    {
+        public Guid Id { get; set; }
+        public Guid OrderId { get; set; }
+        public PaymentMethod Method { get; set; }
+        public PaymentStatus Status { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public Orders Order { get; set; } = null!;
+        public Payments() { }
+
+        public Payments(Guid orderId, PaymentMethod method, PaymentStatus status, decimal amount)
+        {
+            Id = Guid.NewGuid();
+            OrderId = orderId;
+            Method = method;
+            Status = status;
+            Amount = amount;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+    }
+}
