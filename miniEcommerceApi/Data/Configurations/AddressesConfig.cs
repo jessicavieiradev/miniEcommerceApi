4237cd2dc1a07bfefc634a61518a83baa8be1920
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-03-20 10:37:02 -0300

    refactor: rename Customers and Addresses configurations to match updated entity names

---
+++ miniEcommerceApi/Data/Configurations/AddressesConfig.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using miniEcommerceApi.Models;
+
+namespace miniEcommerceApi.Datx.Configurations
+{
+    public class AddressesConfig : IEntityTypeConfiguration<Addresses>
+    {
+        public void Configure(EntityTypeBuilder<Addresses> builder)
+        {
+            builder.ToTable("Addresses");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.ZipCode)
+               .IsRequired()
+               .HasMaxLength(20);
+            builder.Property(x => x.Street)
+                   .IsRequired()
+                   .HasMaxLength(200);
+            builder.Property(x => x.Number)
+                   .HasMaxLength(20);
+            builder.Property(x => x.Neighborhood)
+                   .HasMaxLength(100);
+            builder.Property(x => x.City)
+                   .IsRequired()
+                   .HasMaxLength(100);
+            builder.Property(x => x.State)
+                   .IsRequired()
+                   .HasMaxLength(50);
+            builder.HasOne(x => x.Customer)
+                .WithMany()
+                .HasForeignKey(x => x.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ miniEcommerceApi/Data/Configurations/CustomersConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using miniEcommerceApi.Models;
+
+namespace miniEcommerceApi.Data.Configurations
+{
+    public class CustomersConfig : IEntityTypeConfiguration<Customers>
+    {
+        public void Configure(EntityTypeBuilder<Customers> builder)
+        {
+            builder.ToTable("Customers");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.FirstName).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.LastName).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Phone).IsRequired().HasMaxLength(20);
+            builder.Property(x => x.Cpf).IsRequired().HasMaxLength(20);
+            builder.HasOne(x => x.User)
+                   .WithOne()
+                   .HasForeignKey<Customers>(x => x.UserId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
