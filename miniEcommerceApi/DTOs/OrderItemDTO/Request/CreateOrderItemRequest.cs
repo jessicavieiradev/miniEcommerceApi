bf96406c326c5bf1f5df9bbbead0d26eec2b2475
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-03-20 10:48:36 -0300

    feat: add OrderItem DTOs for request and response

---
+++ miniEcommerceApi/DTOs/OrderItemDTO/Request/CreateOrderItemRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace miniEcommerceApi.DTOs.OrderItemDTO.Request
+{
+    public record CreateOrderItemRequest
+    {
+
+        [Required(ErrorMessage = "ProductId is required")]
+        public Guid ProductId { get; init; }
+
+        [Required(ErrorMessage = "Quantity is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than zero")]
+        public int Quantity { get; init; }
+        public CreateOrderItemRequest() { }
+
+        public CreateOrderItemRequest(Guid productId, int quantity)
+        {
+            ProductId = productId;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ miniEcommerceApi/DTOs/OrderItemDTO/Response/OrderItemResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace miniEcommerceApi.DTOs.OrderItemDTO.Response
+{
+    public record OrderItemResponse
+    {
+        public Guid Id { get; init; }
+        public Guid ProductId { get; init; }
+        public string ProductName { get; init; }
+        public int Quantity { get; init; }
+        public decimal UnitPrice { get; init; }
+        public decimal SubTotal => UnitPrice * Quantity;
+    }
+}
