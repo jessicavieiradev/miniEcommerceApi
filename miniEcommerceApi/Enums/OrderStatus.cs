059900de9e3030c5ac1611bbf53f72cd74734f0a
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-03-20 10:54:32 -0300

    feat: add new OrderStatus values to enum

--- miniEcommerceApi/Enums/OrderStatus.cs
+++ miniEcommerceApi/Enums/OrderStatus.cs
@@ -3,9 +3,10 @@
     public enum OrderStatus
     {
         Pending = 1,
-        Processing = 2,
-        Shipped = 3,
-        Delivered = 4,
-        Cancelled = 5
+        Confirmed = 2,
+        Preparing = 3,
+        Shipped = 4,
+        Delivered = 5,
+        Cancelled = 6
     }
 }
